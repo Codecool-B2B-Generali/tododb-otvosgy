d8409b6c3443dbdb7b654bbc8558685985d2100c
otvosgy <89573142+otvosgy@users.noreply.github.com>
2021-09-09 11:56:12 +0200

    Update task-5.sql

--- task-5.sql
+++ task-5.sql
@@ -1,1 +1,5 @@
 -- Update John's tasks and mark them as done
+update todoschema._todo 
+set DONE='true'
+where id in (select t.id from todoschema._user u, todoschema._todo t
+                where u.id=t.user_id and u.nev ='John');
