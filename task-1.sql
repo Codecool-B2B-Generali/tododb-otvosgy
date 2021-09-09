5beed71a7d931f3f7d4d9276caef89fd0157bcd5
otvosgy <89573142+otvosgy@users.noreply.github.com>
2021-09-09 11:53:40 +0200

    Update task-1.sql

--- task-1.sql
+++ task-1.sql
@@ -1,1 +1,19 @@
 -- Create two tables, one to store the users and one for the todos
+DROP TABLE todoschema._TODO;
+DROP TABLE todoschema._USER;
+DROP schema todoschema;
+
+CREATE schema todoschema;
+
+
+CREATE TABLE todoschema._USER (
+    id int IDENTITY(1,1) PRIMARY KEY,
+    nev VARCHAR(50) NOT NULL
+);
+
+ 
+CREATE TABLE todoschema._TODO (
+    id int IDENTITY(1,1) PRIMARY KEY,
+    feladat VARCHAR(100) NOT NULL,
+	user_id int NOT NULL FOREIGN KEY (user_id) REFERENCES todoschema._USER(ID)                      
+);
